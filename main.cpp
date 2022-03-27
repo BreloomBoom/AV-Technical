608260b51f50f3b303b4cf54fa6aadc46da4cc52
Ronald Chiang <rschaing@gmail.com>
2022-03-27 17:32:52 +1100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,25 @@
-// Question 1 Install g tests and add to cmakes lists
-#include "gtest/gtest.h"
-
+// Question 1 Install g tests and add to cmakes lists X
+#include <gtest/gtest.h>
+#include <iostream>
+#include <map>
+#include <thread>
+#include <cmath> 
+#include <mutex>
+#include <utility>
+#include <assert.h>
 
 const int values[] = { 1,2,3,4,5 };
-const int NVALS = sizeof values / sizeof (int);
+const int NVALS = sizeof (values) / sizeof (int);
 void function(char**);
+void fn(int**);
 
 template <typename T>
 struct ptr_holder {
-  static_assert(std::is_same_v<T, std::remove_pointe<T>>);
+  static_assert(!std::is_same<T, std::remove_pointer<T>>::value);
+  T ptr;
 };
 
-int total = 0
+int total = 0;
 
 class Wallet
 {
@@ -28,111 +36,100 @@ public:
     }
 };
 
-struct {
+class Foo {
+public:
+    Foo(int a, Wallet* b = NULL);
+};
+
+struct person {
    int age;
    float weight;
-} person;
+};
 
-int testerFunction()
-{
-   Wallet walletObject;
-   std::vector<std::thread> threads;
-   for(int i = 0; i < 5; ++i){
-        threads.push_back(std::thread(&Wallet::addMoney, &walletObject, 1000));
-   }
-   for(int i = 0; i < threads.size() ; i++)
-   {
-       threads.at(i).join();
-   }
-   return walletObject.getMoney();
+TEST(WalletTest, ThreadTest) {
+    for (int k = 0; k < 1000; k++) {
+        Wallet walletObject;
+        std::vector<std::thread> threads;
+        for(int i = 0; i < 5; ++i){
+                threads.push_back(std::thread(&Wallet::addMoney, &walletObject, 1000));
+        }
+        for(int i = 0; i < threads.size() ; i++)
+        {
+            threads.at(i).join();
+        }
+        EXPECT_TRUE(walletObject.getMoney()==5000) << "Error at count = "<< k <<" Money in Wallet = "<< walletObject.getMoney() << std::endl;
+    }
 }
-
 
 int Main()
 {
-    // Question 2
-    int x = 10;
-    int y = 15;
-    cout << " "<< (x, y) << std::endl;
+    // Question 2 X
+    int x{10};
+    int y{15};
+    std::cout << x << " " << y << std::endl;
 
-    // Question 3 Get the function to run
-    char arr[] = { "ant", "bat", "cat", "dog", "egg", "fly" };
+    // Question 3 Get the function to run X
+    char *arr[] = { "ant", "bat", "cat", "dog", "egg", "fly" };
 	function(arr);
 
-
     // Question 4 Insert in map
-    typedef map<int, double> valmap;
+    typedef std::map<int, double> valmap;
 
     valmap m;
 
-    for (int i = 0; i < NVALS; i++)
-        m.insert(make_pair(values[i], pow(values[i], .5)));
-
-    valmap::iterator it = 100;              
-    valmap::iterator it2(100);              
-    m.insert(1,2);
+    for (int i = 0; i < NVALS; i++) {
+        m.insert(std::make_pair(values[i], std::pow(values[i], .5)));
+    }
+    valmap::iterator it;              
+    valmap::iterator it2;              
+    m.insert(std::make_pair(1,2));
     
-    // Question 5
+    // Question 5 X
     int i = 1, j = 1;
-    if ( (i =! 3) && (j==1))
+    if ( (i != 3) && (j==1))
     {
-        cout << "inside if statement\n";
+        std::cout << "inside if statement\n";
     }
 
     // Question 6 Store a char * pointer in template
-    ptr_holder<(void *)> p;
+    ptr_holder<char *> p;
 
     // Question 7 Fix the compiler errors and race conditions
     // Convert the testerFunction() into a google test
-    int val = 0;
-    for(int k = 0; k < 1000; k++)
-    {
-        if((val = testerFunction()) != 5000)
-        {
-            std::cout << "Error at count = "<<k<<" Money in Wallet = "<<val << std::endl;
-            return 1;
-        }
-    }
+    
 
-    // Question 8
+    // Question 8 X
     int n = 1;
-    int *p = &n;
-    fn(&p);
+    int *nn = &n;
+    fn(&nn);
 
-    // Question 9
+    // Question 9 X
     struct person *ptr;
     ptr = (struct person*)malloc(sizeof(struct person));
     ptr->age = 10;
     ptr->weight = 55.5;
 
-    // Question 10 Initialise foo
-    Foo foo;
+    // Question 10 Initialise foo X
+    Foo foo(0);
 
-    // Question 11
+    // Question 11 X
     char c;
-    while(c = getchar() == EOF)
+    while((c = getchar()) == EOF)
     {
         putchar(c);
     }
     return 0;                         
-
 }
 
 // Do not change anything besides the ptr variable
-void function(char ptr)
+void function(char **ptr)
 {
 	char *ptr1;
 	ptr1 = (ptr += sizeof(int))[-2];
 	printf("%s\n", ptr1);
 }
 
-void fn(const int** pp)
+void fn(int** pp)
 {
     printf("%p : %p : %d", pp, *pp, **pp);
 }
-
-// Fix any compilation error
-class Foo {
-public:
-    Foo(int a, Wallet* b = NULL);
-};
